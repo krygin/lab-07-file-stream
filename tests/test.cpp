75f21f8e39e57fc834787eb7d16340ca464cf9d4
Andrey Borodin <bo.ro.da@mail.ru>
2019-10-31 10:03:28 +0300

    prepared lab 0&

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,163 @@
 // Copyright 2020 Your Name <your_email>
 
+
 #include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+#include <map>
+#include <string>
+#include <vector>
+
+#include <header.hpp>
+
+bool operator==(const Book& a, const Book& b)
+{
+    return a.Author == b.Author &&
+        a.Title == b.Title &&
+        a.Year == b.Year;
+}
+
+bool operator==(const Student& a, const Student& b)
+{
+    return a.Name == b.Name &&
+        a.Year == b.Year &&
+        a.RecordBook == b.RecordBook;
+}
+
+TEST(TestLab, Student) {
+  std::vector<Student> students1 = {
+    {"Anton", 1, {
+      {"a", Unsatisfactorily },
+      {"b", Excellent},
+      {"c", Satisfactorily}}
+    },
+    {"Slava", 11, {
+      {"a", Unsatisfactorily },
+      {"b", Unsatisfactorily },
+      {"c", Unsatisfactorily }}
+    },
+    {"Masha", 2, {
+      {"a", Excellent},
+      {"c", Satisfactorily}}
+    },
+  };
+
+  std::vector<Student> students2 = {
+    {"Ilya", 1, {
+      {"a", Unsatisfactorily },
+      {"b", Excellent},
+      {"c", Satisfactorily}}
+    },
+    {"Nikita", 11, {
+      {"a", Satisfactorily },
+      {"b", Satisfactorily },
+      {"c", Good }}
+    },
+    {"David", 2, {
+      {"a", Excellent},
+      {"c", Good}}
+    },
+  };
+
+  Groups groups = { {"iu8-16", students1}, {"iu8-11", students2} };
+
+  saveToFile("file1", groups);
+  Groups out;
+  loadFromFile("file1", out);
+  EXPECT_EQ(groups, out);
+
+  loadFromFile("file2", out);
+  EXPECT_TRUE(out.empty());
+}
+
+TEST(TestLab, SaveBooks) {
+    std::vector<Book> books = {
+      {"Marks and Co", "Capital", 1867},
+      {"Mayers, Scott", "C++", 1997},
+      {"Lenin", "The Best", 1905},
+      {"Tolstoy",
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaa!№;%:?*(){:><?}[]';./,aaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ",
+       1836}
+    };
+
+    saveToFile("file1", books);
+    std::vector<Book> out;
+    loadFromFile("file1", out);
+    EXPECT_EQ(books, out);
+
+    loadFromFile("file2", out);
+    EXPECT_TRUE(out.empty());
+}
+
+TEST(TestLab, SaveStrings) {
+    std::vector<std::string> data = {
+      "string", "str", "empty", "", "",
+      "a string", "number 13 string", "a small string",
+      "%!@#$%^&*(){:>>>?\"}", "and a big string",
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ",
+      ";)"
+    };
+
+    saveToFile("file1", data);
+    std::vector<std::string> out;
+    loadFromFile("file1", out);
+    EXPECT_EQ(data, out);
+
+    loadFromFile("file2", out);
+    EXPECT_TRUE(out.empty());
 }
